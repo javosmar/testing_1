bfa698b47c97d9351f1ca340ebf0f5b37caadb0b
Martin Acosta <javosmar@gmail.com>
2020-11-12 14:49:45 -0300

    se ordena la lista de elementos

--- test/test_lista.js
+++ test/test_lista.js
@@ -8,8 +8,8 @@
  * v Cuando se agrega un elemento a una lista vacía hay un elemento.
  * v Cuando se agrega un elemento a una lista vacía se puede recuperar el valor a partir de la clave.
  * v Cuando se agrega una clave que ya está en la lista se actualiza el valor correspondiente.
- * Cuando se agrega una clave que ya está en la lista no cambia la cantidad de elementos almacenados.
- * Cuando se agregan un elemento a una lista vacía la lista de claves esta ordenada.
+ * v Cuando se agrega una clave que ya está en la lista no cambia la cantidad de elementos almacenados.
+ * Cuando se agrega un elemento a una lista vacía la lista de claves esta ordenada.
  * Cuando se agrega un elemento al principio la lista de claves esta ordenada.
  * Cuando se agrega un elemento al final la lista de claves esta ordenada.
  * Si agrego dos elementos a la lista entonces puedo buscar a cada uno de los valores.
@@ -19,38 +19,52 @@ const assert = require("chai").assert;
 const { expect } = require("chai");
 const Lista = require("../src/lista.js");
 
-describe("en una lista vacia" , function() {
+describe("en una lista vacia", function () {
     var lista = new Lista();
 
-    it("hay cero elementos", function() {
+    it("hay cero elementos", function () {
         assert.equal(lista.count(), 0);
     });
 
-    it("no se encuentra ninguna clave", function() {
+    it("no se encuentra ninguna clave", function () {
         assert.isNaN(lista.find("clave"));
     });
 
 });
 
-describe("cuando se agrega un elemento a una lista vacia", function() {
+describe("cuando se agrega un elemento a una lista vacia", function () {
     let lista = new Lista();
     lista.add("clave", "valor");
 
-    it("hay un elemento", function() {
+    it("hay un elemento", function () {
         assert.equal(lista.count(), 1);
     });
 
-    it("se puede recuperar el valor a partir de la clave", function() {
+    it("se puede recuperar el valor a partir de la clave", function () {
         assert.equal(lista.find("clave"), "valor");
     });
+
+    it('la lista de claves esta ordenada.', () => {
+        lista.add("aclave", "valor");
+        lista.ordenar();
+        const ordenado = lista.isOrdenado();
+        expect(ordenado).to.be.true;
+    })
 });
 
-describe("Cuando se agrega una clave que ya está en la lista ", function() {
+describe("Cuando se agrega una clave que ya está en la lista ", function () {
     let lista = new Lista();
     lista.add("clave", "valor");
-    lista.add("clave", "valor2");
 
-    it("se actualiza el valor correspondiente", function() {
+    it("se actualiza el valor correspondiente", function () {
+        lista.add("clave", "valor2");
         assert.equal(lista.find("clave"), "valor2");
-    });;
+    });
+
+    it('no cambia la cantidad de elementos almacenados', () => {
+        const cantidadPrevia = lista.count();
+        lista.add("clave", "valor2");
+        const cantidadNueva = lista.count();
+        expect(cantidadNueva).to.be.equal(cantidadPrevia);
+    });
 });
